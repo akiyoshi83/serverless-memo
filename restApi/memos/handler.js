16ea2821920930ba03262e28bb3bc9650c1ba7fc
akiyoshi83 <mail@akiyoshi.info>
2016-05-03 23:04:03 +0900

    Add APIGateway headers

--- restApi/memos/handler.js
+++ restApi/memos/handler.js
@@ -3,6 +3,7 @@
 module.exports.handler = function(event, context, cb) {
   return cb(null, {
     message: 'Go Serverless! Your Lambda function executed successfully!',
+    apig: event.apig,
     title: event.title,
     body: event.body
   });
