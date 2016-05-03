c413257bebaeb5483e416f6c019e6fe33e4da19d
akiyoshi83 <mail@akiyoshi.info>
2016-05-03 20:13:09 +0900

    Change memos api structure

---
+++ restApi/memos/handler.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports.handler = function(event, context, cb) {
+  return cb(null, {
+    message: 'Go Serverless! Your Lambda function executed successfully!',
+    title: event.title,
+    body: event.body
+  });
+};
