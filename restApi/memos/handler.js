24865b2f26388a11635d0de03d273a458fdef71c
akiyoshi83 <mail@akiyoshi.info>
2016-05-04 00:50:16 +0900

    Implement create memo

--- restApi/memos/handler.js
+++ restApi/memos/handler.js
@@ -1,10 +1,38 @@
 'use strict';
 
+var AWS = require('aws-sdk');
+var dynamodb = new AWS.DynamoDB({region: process.env['SERVERLESS_REGION']});
+var stage = process.env['SERVERLESS_STAGE'];
+var memosTableName = "Memos-" + stage;
+
 module.exports.handler = function(event, context, cb) {
-  return cb(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!',
-    apig: event.apig,
-    title: event.title,
-    body: event.body
-  });
+  //console.debug(JSON.stringify(event.apig));
+
+  var method = event.apig.httpMethod;
+  var now = new Date();
+
+  if (method === 'POST') {
+    var params = {
+      TableName: memosTableName,
+      Item: {
+        UserId: { "S": "anonymous" },
+        Title: { "S": event.title },
+        Body: { "S": event.body },
+        CreatedAt: { "N": now.getTime().toString() },
+        UpdatedAt: { "N": now.getTime().toString() }
+      }
+    };
+    dynamodb.putItem(params, function(err, data) {
+      if (err) {
+        console.error("Create Memo Failed:", err, err.stack);
+      } else {
+        console.log("Create Memo:", params.Item.UserId, params.Item.CreatedAt);
+      }
+      return cb(err, data);
+    });
+  } else {
+    return cb(null, {
+      message: 'Not supported method'
+    });
+  }
 };
